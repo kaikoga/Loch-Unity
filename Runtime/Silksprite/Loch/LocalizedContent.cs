c03ae37a25681fdeaf0de87d41cdd32b8b97632c
kaikoga <info@mail.kaikoga.net>
2026-04-12 23:21:53 +0900

    struct Loc

--- Runtime/Silksprite/Loch/LocalizedContent.cs
+++ Runtime/Silksprite/Loch/LocalizedContent.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using JetBrains.Annotations;
 using Silksprite.Loch.Core;
+using Unity.Burst;
 using UnityEngine;
 
 namespace Silksprite.Loch
 {
     [PublicAPI]
-    public class LocalizedContent : IEquatable<LocalizedContent>
+    [SuppressMessage("ReSharper", "Unity.BurstFunctionSignatureContainsManagedTypes")]
+    [SuppressMessage("ReSharper", "Unity.BurstLoadingManagedType")]
+    public readonly struct LocalizedContent : IEquatable<LocalizedContent>
     {
         readonly string _key;
         internal readonly Assembly Assembly;
@@ -28,35 +32,26 @@ namespace Silksprite.Loch
         public string LongTrFormat(Substitution substitution) => substitution.Format(LochRepository.Instance.LongTr(_key, Assembly));
         public GUIContent GUIContentFormat(Substitution substitution) => new GUIContent(substitution.Format(LochRepository.Instance.Tr(_key, Assembly)));
 
-        public bool Equals(LocalizedContent? other)
+
+        public bool Equals(LocalizedContent other)
         {
-            if (other is null)
-                return false;
-            if (ReferenceEquals(this, other))
-                return true;
             return _key == other._key && Assembly.Equals(other.Assembly);
         }
         public override bool Equals(object? obj)
         {
-            if (obj is null)
-                return false;
-            if (ReferenceEquals(this, obj))
-                return true;
-            if (obj.GetType() != GetType())
-                return false;
-            return Equals((LocalizedContent)obj);
+            return obj is LocalizedContent other && Equals(other);
         }
         public override int GetHashCode()
         {
             return HashCode.Combine(_key, Assembly);
         }
-        public static bool operator ==(LocalizedContent? left, LocalizedContent? right)
+        public static bool operator ==(LocalizedContent left, LocalizedContent right)
         {
-            return Equals(left, right);
+            return left.Equals(right);
         }
-        public static bool operator !=(LocalizedContent? left, LocalizedContent? right)
+        public static bool operator !=(LocalizedContent left, LocalizedContent right)
         {
-            return !Equals(left, right);
+            return !left.Equals(right);
         }
     }
 }
