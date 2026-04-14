6bfde1c4532bd4e07d69dd6dfc1cbb38385c2bb6
kaikoga <info@mail.kaikoga.net>
2026-04-15 01:48:15 +0900

    struct Substitution

--- Runtime/Silksprite/Loch/LocalizedContent.cs
+++ Runtime/Silksprite/Loch/LocalizedContent.cs
@@ -3,39 +3,52 @@ using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using JetBrains.Annotations;
 using Silksprite.Loch.Core;
-using Unity.Burst;
 using UnityEngine;
 
 namespace Silksprite.Loch
 {
     [PublicAPI]
+    [SuppressMessage("ReSharper", "Unity.BurstAccessingManagedMethod")]
     [SuppressMessage("ReSharper", "Unity.BurstFunctionSignatureContainsManagedTypes")]
     [SuppressMessage("ReSharper", "Unity.BurstLoadingManagedType")]
     public readonly struct LocalizedContent : IEquatable<LocalizedContent>
     {
         readonly string _key;
         internal readonly Assembly Assembly;
+        internal readonly ReadOnlySubstitution Substitution;
 
         public LocalizedContent(string key, Assembly assembly)
         {
             _key = key;
             Assembly = assembly;
+            Substitution = ReadOnlySubstitution.Empty;
         }
 
-        public string Tr => LochRepository.Instance.Tr(_key, Assembly);
-        public string? TryTr => LochRepository.Instance.TryTr(_key, Assembly);
-        public string LongTr => LochRepository.Instance.LongTr(_key, Assembly);
-        public string? TryLongTr => LochRepository.Instance.TryLongTr(_key, Assembly);
-        public GUIContent GUIContent => LochRepository.Instance.GUIContent(_key, Assembly);
+        public LocalizedContent Format(Substitution substitution) => new LocalizedContent(_key, Assembly, substitution);
 
-        public string TrFormat(Substitution substitution) => substitution.Format(LochRepository.Instance.Tr(_key, Assembly));
-        public string LongTrFormat(Substitution substitution) => substitution.Format(LochRepository.Instance.LongTr(_key, Assembly));
-        public GUIContent GUIContentFormat(Substitution substitution) => new GUIContent(substitution.Format(LochRepository.Instance.Tr(_key, Assembly)));
+        LocalizedContent(string key, Assembly assembly, Substitution substitution)
+        {
+            _key = key;
+            Assembly = assembly;
+            Substitution = substitution.ToReadOnly();
+        }
 
+        public string Tr => MaybeFormat(LochRepository.Instance.Tr(_key, Assembly));
+        public string? TryTr => TryMaybeFormat(LochRepository.Instance.TryTr(_key, Assembly));
+        public string LongTr => MaybeFormat(LochRepository.Instance.LongTr(_key, Assembly));
+        public string? TryLongTr => TryMaybeFormat(LochRepository.Instance.TryLongTr(_key, Assembly));
+        public GUIContent GUIContent => Substitution.HasValue ? new GUIContent(Tr) : LochRepository.Instance.GUIContent(_key, Assembly);
+
+        string MaybeFormat(string value) => Substitution.HasValue ? Substitution.Format(value) : value;
+        string? TryMaybeFormat(string? value) => value != null ? MaybeFormat(value) : null;
+        
+        public string TrFormat(Substitution substitution) => substitution.Format(Tr);
+        public string LongTrFormat(Substitution substitution) => substitution.Format(LongTr);
+        public GUIContent GUIContentFormat(Substitution substitution) => new GUIContent(substitution.Format(Tr));
 
         public bool Equals(LocalizedContent other)
         {
-            return _key == other._key && Assembly.Equals(other.Assembly);
+            return _key == other._key && Assembly.Equals(other.Assembly) && Equals(Substitution, other.Substitution);
         }
         public override bool Equals(object? obj)
         {
@@ -43,7 +56,7 @@ namespace Silksprite.Loch
         }
         public override int GetHashCode()
         {
-            return HashCode.Combine(_key, Assembly);
+            return HashCode.Combine(_key, Assembly, Substitution);
         }
         public static bool operator ==(LocalizedContent left, LocalizedContent right)
         {
--- Runtime/Silksprite/Loch/Substitution.cs
+++ Runtime/Silksprite/Loch/Substitution.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using JetBrains.Annotations;
@@ -6,36 +5,57 @@ using JetBrains.Annotations;
 namespace Silksprite.Loch
 {
     [PublicAPI]
-    public class Substitution
+    public struct Substitution
     {
-        static readonly Regex Pattern = new Regex("{([^}]*)}");
-        
-        public static readonly Substitution Empty = new Substitution();
+        public static readonly Substitution Empty;
 
-        readonly Dictionary<string, string> _dict;
-        readonly Func<string, string> _mapping;
+        Dictionary<string, string>? _dict;
+        Dictionary<string, string> Dict => _dict ??= new Dictionary<string, string>();
+
+        public bool HasValue => _dict != null;  
 
         public string this[string key]
         {
-            set => _dict[key] = value;
+            set => Dict[key] = value;
         }
 
-        public Substitution(Func<string, string>? mapping = null) : this(new Dictionary<string, string>(), mapping) { }
-
-        Substitution(Dictionary<string, string> dict, Func<string, string>? mapping = null)
+        public Substitution Merge(Substitution other)
         {
-            _dict = dict;
-            _mapping = mapping ?? (key => $"{{{key}}}");
+            if (other._dict is { } dict)
+            {
+                foreach (var key in dict)
+                {
+                    Dict.Add(key.Key, key.Value);
+                }
+            }
+            return this;
         }
 
+        public string Format(string value) => ToReadOnly().Format(value);
+
+        internal ReadOnlySubstitution ToReadOnly() => new ReadOnlySubstitution(_dict);
+    }
+
+    readonly struct ReadOnlySubstitution
+    {
+        public static readonly ReadOnlySubstitution Empty;
+
+        static readonly Regex Pattern = new Regex("{([^}]*)}");
+
+        readonly Dictionary<string, string>? _dict;
+
+        public bool HasValue => _dict != null;  
+
+        public ReadOnlySubstitution(Dictionary<string, string>? dict) => _dict = dict;
+        
         public string Format(string value)
         {
-            var mapping = _mapping;
-            return Pattern.Replace(value, match =>
-            {
-                var key = match.Groups[1].Value;
-                return _dict.TryGetValue(key, out var v) ? v : mapping(key);
-            });
+            return _dict is { } dict ? Pattern.Replace(value, match =>
+                {
+                    var key = match.Groups[1].Value;
+                    return dict.TryGetValue(key, out var v) ? v : $"{{{key}}}";
+                })
+                : value;
         }
     }
 }
